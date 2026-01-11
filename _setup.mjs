c84c83985c9babfdbd4cfc5c8f0319f3ad67b3fd
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-01-11 12:48:17 +0100

    Create _setup.mjs

---
+++ _setup.mjs
@@ -0,0 +1,7 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+//code
+
+process.exit();
