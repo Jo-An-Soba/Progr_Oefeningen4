1740a6e923a24c461eba789bc382a805a4690e31
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-01-11 13:32:32 +0100

    Update 6arrays_E1-studentenlijst.mjs

--- 6arrays_E1-studentenlijst.mjs
+++ 6arrays_E1-studentenlijst.mjs
@@ -2,6 +2,17 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-//code
+let array = [];
+
+let namen = await userInput.question("Geef lijst namen op: ")
+array.push(namen)
+
+while (namen != ""){
+namen = await userInput.question("Geef lijst namen op: ")
+array.push(namen)
+
+}
+
+console.log(array)
 
 process.exit();
