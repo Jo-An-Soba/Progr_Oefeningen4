f65de8d91b3b7dd7d2def25b61394b24c7281c62
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-01-12 07:05:09 +0100

    Update 6arrays_E1-studentenlijst.mjs

--- 6arrays_E1-studentenlijst.mjs
+++ 6arrays_E1-studentenlijst.mjs
@@ -2,13 +2,7 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
-/*let straal = parseFloat(await userInput.question("straal? "))
 
-function circel(straal) {
-    return Math.PI * straal
-}
-let oppCircel = circel(straal);
-console.log(oppCircel)*/
 
 
 let lijstNamen = [];
