516cf5b35b069042b39dc6f0993c3a8504b1969e
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-01-12 11:16:28 +0100

    nog

--- 7objecten_E3-Leeftijden.mjs
+++ 7objecten_E3-Leeftijden.mjs
@@ -3,22 +3,23 @@ import { stdin as input, stdout as output } from 'node:process';
 import { clear } from 'node:console';
 const userInput = readline.createInterface({ input, output });
 
-let hond1 = { "naam": "Bella", "leeftijd": 2, "isSpeels": true };
-let hond2 = { "naam": "Ron", "leeftijd": 10, "isSpeels": false }
-
-function begroet (){
-    if(this.isSpeels){
-    console.log("Hallo, ik ben " + this.naam + " ,ik ben " + this.leeftijd + " jaar oud en ik ben speels")
-}else{
-console.log("Hallo, ik ben " + this.naam + " ,ik ben " + this.leeftijd + " jaar oud en ik ben slecht gezind")
+let persoon1 = {
+    "naam": "Jos",
+    "leeftijd": 12
 }
+let persoon2 = {
+    "naam": "Kevin",
+    "leeftijd": 16
 }
 
-hond1.begroet = begroet; //functie als key in object steken
-hond1.begroet(); // funtie aanroepen (betekent: "functie uitvoeren")
-
-hond2.begroet = begroet; 
-hond2.begroet(); 
-
+function vergelijkLeeftijd() {
+    if (persoon1.leeftijd < persoon2.leeftijd) {
+        let verschil = persoon2.leeftijd - persoon1.leeftijd
+        console.log(persoon1.naam + " is de jongste en ze schelen " + verschil + " jaren")
+    } else {
+        console.log("else")
+    }
+}
+vergelijkLeeftijd()
 
 process.exit();
---
+++ 7objecten_M2-Menu.mjs
@@ -0,0 +1,8 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { clear } from 'node:console';
+const userInput = readline.createInterface({ input, output });
+
+
+
+process.exit();
