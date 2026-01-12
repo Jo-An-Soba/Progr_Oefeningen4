8712d1ac01aac726110c86113511af57e7d0dd56
Jo-An-Soba <anicee.decarpentrie@student.ehb.be>
2026-01-12 09:28:24 +0100

    bla

---
+++ 7objecten_E1-Honden.mjs
@@ -0,0 +1,21 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { clear } from 'node:console';
+const userInput = readline.createInterface({ input, output });
+
+let honden = [
+ {    "naam" : "Bella",
+    "leeftijd" : 2,
+    "isSpeels" : true
+},
+
+{
+    "naam" : "Ron",
+    "leeftijd" : 10,
+    "isSpeels" : false
+}
+]
+
+console.log(honden);
+
+process.exit();
